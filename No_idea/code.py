86ddeed39bfd212b0bc3e66dc5b6a988da6b8540
Mohammed Sohail <sohail.shaa786@gmail.com>
2026-01-27 00:44:00 +0530

    commited some changes in code.py

--- No_idea/code.py
+++ No_idea/code.py
@@ -1,13 +1,13 @@
-n,m=map(int,input().split())
-array_size=list(map(int,input().split()))
-set_A=set(map(int,input().split()))
+n,m=map(int,input().split()) # creating a variable called n,m n=interger and m=size of sets
+array_size=list(map(int,input().split())) # its an array size
+set_A=set(map(int,input().split())) # conatining A set mapped into int data type and slpited for both A and B sets
 set_B=set(map(int,input().split()))
 
-happiness=0
+happiness=0 # happiness just a score
 
 for i in array_size:
     if i in set_A:
-        happiness+=1
+        happiness+=1 # incresing number
     elif i in set_B:
         happiness-=1
 print(happiness)
